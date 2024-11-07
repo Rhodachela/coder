0b2db313b2f61a37b406d9032aea71593911e102
Rhoda <rhodachela@gmail.com>
2024-11-07 23:20:29 +0300

    Main file

---
+++ inventory_management_system/main.py
@@ -0,0 +1,8 @@
+from classes import Product, InventoryManager
+
+manager = InventoryManager()
+# new_product = Product(name= "Raha premium", category= "Maize flour", stock_quantity=5, price= 1500)
+# manager.add_product(new_product)
+manager.view_all_products()
+
+# manager.delete_product()
