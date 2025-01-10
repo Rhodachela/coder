f591e7d8bd813a20f747c560d15e0b7bb325fe39
Rhoda <rhodachela@gmail.com>
2025-01-10 14:30:37 +0300

    Users app configurations

--- MY-PROJECT/users/serializers.py
+++ MY-PROJECT/users/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
 from .models import CustomUser
-
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email']
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Hash the password before saving the user
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
