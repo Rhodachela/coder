c4640716c88f1cb550333869b4a44d49142a38b4
Rhoda <rhodachela@gmail.com>
2025-01-10 14:29:57 +0300

    Products file

--- MY-PROJECT/products/serializers.py
+++ MY-PROJECT/products/serializers.py
@@ -9,22 +9,34 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer()  # Nested serializer to show category details
-    reviews = serializers.SerializerMethodField()  # Custom field to display reviews
-    is_in_stock = serializers.BooleanField(source='is_in_stock', read_only=True)
+    category = serializers.CharField()  # Accept a category name directly as a string
 
     class Meta:
         model = Product
         fields = [
-            'id', 'name', 'description', 'price', 'stock_quantity', 'category', 
-            'image_url', 'created_at', 'reviews', 'is_in_stock'
+            'id',
+            'name',
+            'description',
+            'price',
+            'stock_quantity',
+            'category',
+            'image_url',
+            'created_at',
+            'is_in_stock',
         ]
+
+    def create(self, validated_data):
+        # Handle category creation or retrieval
+        category_name = validated_data.pop('category')
+        category, created = Category.objects.get_or_create(name=category_name)
+        validated_data['category'] = category
+        return super().create(validated_data)
 
     def get_reviews(self, obj):
         """Retrieve the reviews for a product."""
         reviews = obj.reviews.all()
         return ReviewSerializer(reviews, many=True).data
-        
+
 class ProductWriteSerializer(serializers.ModelSerializer):
     """Serializer for creating and updating Product instances."""
     class Meta:
